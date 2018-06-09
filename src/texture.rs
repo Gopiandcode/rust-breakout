8c1067d2a13302fafc45725601b6b91b76bb7b34
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-06-09 11:40:22 +0100

    completed texture class

---
+++ src/texture.rs
@@ -0,0 +1,67 @@
+extern crate gl;
+use gl::types::{GLuint, GLint};
+use std::os::raw::c_void;
+
+
+/// Struct used to represent texture
+pub struct Texture {
+    id : GLuint,              // id of texture object
+    width: GLuint,            // width of loaded texture
+    height: GLuint,           // height of loaded texture
+    internal_format: GLuint,  // format of texture object
+    image_format: GLuint,     // format of loaded image
+    wrap_S: GLuint,           // wrapping on S axis
+    wrap_T: GLuint,           // wrapping on T axis
+    filter_min: GLuint,       // filtering mode if pixels < screen pixels
+    filter_max: GLuint,       // filtering mode if pixels > screen pixels
+}
+
+
+impl Texture {
+    pub fn new() -> Self {
+        let mut id = 0;
+        unsafe {
+            gl::GenTextures(1 ,&mut id);
+        }
+
+        Texture{
+            id: id,
+            width: 0,
+            height: 0,
+            internal_format: gl::RGB,
+            image_format: gl::RGB,
+            wrap_S: gl::REPEAT,
+            wrap_T: gl::REPEAT,
+            filter_min: gl::LINEAR,
+            filter_max: gl::LINEAR
+        }
+    }
+
+
+    pub unsafe fn generate(&mut self, width: GLuint, height: GLuint, data: *const c_void) {
+
+        self.width = width;
+        self.height = height;
+        gl::BindTexture(gl::TEXTURE_2D, self.id);
+            gl::TexImage2D(gl::TEXTURE_2D,
+                       0,
+                       self.internal_format as GLint,
+                       width as GLint, height as GLint,
+                       0,
+                       self.image_format,
+                       gl::UNSIGNED_BYTE,
+                       data);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, self.wrap_S as GLint);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, self.wrap_T as GLint);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, self.filter_min as GLint);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, self.filter_max as GLint);
+        gl::BindTexture(gl::TEXTURE_2D, 0);
+    }
+
+
+    pub unsafe fn bind(&self) {
+        gl::BindTexture(gl::TEXTURE_2D, self.id);
+    }
+
+}
+
